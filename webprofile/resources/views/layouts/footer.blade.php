c337ac22c1b9bd3dbd133f680e5480977a9d3833
Cuong <talentandstyle@gmail.com>
2020-07-19 00:54:00 +0700

    create layout page

---
+++ webprofile/resources/views/layouts/footer.blade.php
@@ -0,0 +1,47 @@
+<!-- Footer -->
+<footer class="page-footer font-small fixed-bottom">
+
+    <!-- Footer Elements -->
+    <div class="container">
+
+        <!-- Social buttons -->
+        <ul class="list-unstyled list-inline text-center">
+            <li class="list-inline-item">
+                <a class="btn-floating btn-fb mx-1">
+                    <i class="fa fa-facebook-f"></i>
+                </a>
+            </li>
+            <li class="list-inline-item">
+                <a class="btn-floating btn-tw mx-1">
+                    <i class="fa fa-twitter"> </i>
+                </a>
+            </li>
+            <li class="list-inline-item">
+                <a class="btn-floating btn-gplus mx-1" href="https://github.com/manhcuongdoan/webprofile">
+                    <i class="fa fa-github"></i>
+                </a>
+            </li>
+            <li class="list-inline-item">
+                <a class="btn-floating btn-li mx-1">
+                    <i class="fa fa-linkedin"> </i>
+                </a>
+            </li>
+            <li class="list-inline-item">
+                <a class="btn-floating btn-dribbble mx-1">
+                    <i class="fa fa-dribbble"> </i>
+                </a>
+            </li>
+        </ul>
+        <!-- Social buttons -->
+
+    </div>
+    <!-- Footer Elements -->
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">© 2020 Copyright:
+        <a href="https://mdbootstrap.com/"> Webprofile.com</a>
+    </div>
+    <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
