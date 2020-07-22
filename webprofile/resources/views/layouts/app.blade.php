1da482054d687a03dc072c3e8035115b9720c053
Cuong <talentandstyle@gmail.com>
2020-07-23 01:15:09 +0700

    Change frontend app

--- webprofile/resources/views/layouts/app.blade.php
+++ webprofile/resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container-fluid">
+            <div class="container">
                 <a class="navbar-brand d-flex" href="{{ url('/') }}">
                     <div><img src="/png/Webprofile.png" style="max-height:35px"></div>
                     <div class="pl-2 pt-1">webprofile</div>
